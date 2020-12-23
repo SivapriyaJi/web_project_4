9b5a08673f71f7a14ea7fab011dc2aa92b8ce124
Shivapriya Saraswati <sivapriya.saraswati@gmail.com>
2020-12-22 22:29:52 -0500

    Hotfix: cleanup unused code

--- script/index.js
+++ script/index.js
@@ -154,40 +154,17 @@ addCardModalButton.addEventListener('click', () => {
     togglePopup(imageModalWindow)
   });
 
-  //add card handler
-/*const newCard = document.querySelector(".card-template"); .content.querySelector(".elements__card");*/
 
-/* add card code to be deleted~~~~
-form.addEventListener('submit', formSubmitHandler);
 addCardSubmitButton.addEventListener('click', () => {
-  if (addCardModalWindow.classList.contains('popup_is-open')) {
     const cardElement = cardTemplate.cloneNode(true);
+
     const cardImage = cardElement.querySelector(".elements__img");
     const cardTitle = cardElement.querySelector(".elements__heading");
     const cardLikeButton = cardElement.querySelector(".elements__like-btn");
     const cardDeleteButton = cardElement.querySelector(".elements__delete-btn");
+
     cardTitle.textContent = inputCardTitle.value;
-    cardLink.src = inputCardLink.value;
-    }
-  togglePopup(addCardModalWindow);
-  //list.prepend(cardElement);
-//});*/
-addCardSubmitButton.addEventListener('click', () => {
-      // this line a little bit unclear, are you sure you're getting here relevant fields from your popup?
-    const cardElement = cardTemplate.cloneNode(true);
-
-    const cardImage = cardElement.querySelector(".elements__img");
-    const cardTitle = cardElement.querySelector(".elements__heading");
-    const cardLikeButton = cardElement.querySelector(".elements__like-btn");
-    const cardDeleteButton = cardElement.querySelector(".elements__delete-btn");
-
-      // you're setting your fields for already prepended element
-      cardTitle.textContent = inputCardTitle.value;
-      //cardLink.src = inputCardLink.value;
-      cardImage.src = inputCardLink.value;
-
-     //cardTitle.textContent = data.name;
-     //cardImage.src = data.link;
+    cardImage.src = inputCardLink.value;
 
      function toggleLike() {
        cardLikeButton.classList.toggle('elements__like-btn_active');
@@ -201,45 +178,12 @@ addCardSubmitButton.addEventListener('click', () => {
        cardElement.remove();
      });
 
-    // you're prepending your list, here everything seems to be fine
     list.prepend(cardElement);
-
 
   togglePopup(addCardModalWindow);
 });
 
-/* this is the one I'm trying to make work
-form.addEventListener('submit', formSubmitHandler);
-addCardSubmitButton.addEventListener('click', () => {
-function createCard(card){
-  const cardElement = cardTemplate.cloneNode(true)
-  const link = 'url(' + card.link + ')'
-  const cardImage = cardElement.querySelector(".elements__img")
-  cardImage.style.backgroundImage = link
-  cardElement.querySelector(".elements__heading").textContent = card.name
-  cardElement.querySelector(".elements__like-btn").addEventListener("click", likeImage)
-  cardImage.addEventListener("click", popUpImage)
-  cardElement.querySelector(".elements__delete-btn").addEventListener("click", deleteImage)
-  places.prepend(cardElement)
-}
-createCard(card);
-});*/
 
 
-
-
-
-
-/*  <template class="card-template">
-    <li class="elements__card">
-      <button class ="elements__delete-btn" type="button"></button>
-      <img alt="" src="" class="elements__img">
-       <!-- <div style="" class="elements__img"></div>-->
-      <div class="elements__description">
-        <h2 class="elements__heading"></h2>
-        <button class ="elements__like-btn" type="button"></button>
-      </div>
-    </li>
-  </template>*/
 
 
